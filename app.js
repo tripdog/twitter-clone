de4038c2ab7980e19ade98f89f9158dc7c598570
Tom Phillips <eflips@yahoo.com>
2021-05-29 22:00:11 -0700

    Added error page, post end point

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 const express = require("express");
 const path = require("path")
 const app = express();
+//Now add some middleware to process the url encoded data
+app.use(express.urlencoded({ extended: true }));
+//Now create a post endpoint
 app.set("view engine", "pug")
 app.set("views", path.join(__dirname, "views"))
-
+//Tell the program to watch the root folder for an index page and listen to port 3005
 app.get("/", (req, res) => res.render("index"));
 app.listen(3005, () => console.log("Server is ready Sir!"));
 
+app.post("/", (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    res.render("error", {
+      message: "Please create username and password",
+    });
+    return;
+  }
+  console.log(req.body, username, password);
+  res.end();
+});
