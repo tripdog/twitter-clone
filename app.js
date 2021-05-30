756b0f06df2acdbb0927c7012cd7cb8081de3686
Tom Phillips <eflips@yahoo.com>
2021-05-30 16:44:01 -0700

    The post endpoint is complete now.

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
+const bcrypt = require("bcrypt")
 const express = require("express");
 const path = require("path")
+//set up the database to store and retieve users, passwords
+const redis = require("redis");
 const app = express();
+const client = redis.createClient();
 //Now add some middleware to process the url encoded data
 app.use(express.urlencoded({ extended: true }));
 //Now create a post endpoint
@@ -8,17 +12,39 @@ app.set("view engine", "pug")
 app.set("views", path.join(__dirname, "views"))
 //Tell the program to watch the root folder for an index page and listen to port 3005
 app.get("/", (req, res) => res.render("index"));
-app.listen(3005, () => console.log("Server is ready Sir!"));
 
-app.post("/", (req, res) => {
-  const { username, password } = req.body;
+
+app.post('/', (req, res) => {
+  const { username, password } = req.body
 
   if (!username || !password) {
-    res.render("error", {
-      message: "Please create username and password",
-    });
-    return;
+    res.render('error', {
+      message: "Please create a username AND a password"
+    })
+    return
   }
   console.log(req.body, username, password);
-  res.end();
+ //HGET is a redis command  that returns the value of a field in a hash
+  client.hget('users', username, (err, userid) => {
+    if (!userid) {
+      //user doesn't exist, so a signup process begins
+      client.incr("userid", async (err, userid) => {
+        client.hset("users", username, userid);
+        const saltRounds = 10;
+        const hash = await bcrypt.hash(password, saltRounds);
+        client.hset(`user:${userid}`, "hash", hash, "username", username);
+      });
+    } else {
+      client.hget(`user:${userid}`, 'hash', async (err, hash) => {
+        const result = await bcrypt.compare(password, hash); //returns true or false
+        if (result) {
+          //then the PS is matching
+        } else {
+          //the password is incorrect
+        }
+      })
+    }
+  })
+  res.end()
+  app.listen(3005, () => console.log("Server is ready Sir!"))
 });
