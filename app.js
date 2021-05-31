d2bcca73c2ba283b52a4a16b9593cf50a285b277
Tom Phillips <eflips@yahoo.com>
2021-05-30 20:50:49 -0700

    Fixed error with express (saveUninitialized) etc

--- app.js
+++ app.js
@@ -1,50 +1,86 @@
+const session = require("express-session")
 const bcrypt = require("bcrypt")
 const express = require("express");
 const path = require("path")
-//set up the database to store and retieve users, passwords
+//set up require the database to store and retieve users, passwords
 const redis = require("redis");
 const app = express();
 const client = redis.createClient();
+const RedisStore = require("connect-redis")(session)
 //Now add some middleware to process the url encoded data
 app.use(express.urlencoded({ extended: true }));
-//Now create a post endpoint
+
+app.use(
+  session({
+    store: new RedisStore({ client: client }),
+    resave: true,
+    cookie: {
+      maxAge: 36000000, //10 hours
+      httpOnly: false,
+      secure: false,
+      sameSite: "lax",
+    },
+    secret: "AsecretPWtoValidateAsession0k?",
+    saveUninitialized: true,
+  })
+);
+app.listen(3005, () => console.log("Server is ready Sir!"));
 app.set("view engine", "pug")
 app.set("views", path.join(__dirname, "views"))
 //Tell the program to watch the root folder for an index page and listen to port 3005
-app.get("/", (req, res) => res.render("index"));
+app.get("/", (req, res) => {
+  if (req.session.userid) {
+    res.render("dashboard")
+  } else {
+    res.render("login")
+  }
+});
 
-
+//Now create a post endpoint
 app.post('/', (req, res) => {
   const { username, password } = req.body
-
   if (!username || !password) {
     res.render('error', {
-      message: "Please create a username AND a password"
+      message: "Please create a username AND a password."
     })
     return
   }
+  const saveSessionAndRenderDashboard = (userid) => {
+    req.session.userid = userid
+    req.session.save()
+    res.render('dashboard')
+  }
   console.log(req.body, username, password);
+
  //HGET is a redis command  that returns the value of a field in a hash
   client.hget('users', username, (err, userid) => {
     if (!userid) {
       //user doesn't exist, so a signup process begins
       client.incr("userid", async (err, userid) => {
         client.hset("users", username, userid);
+
         const saltRounds = 10;
         const hash = await bcrypt.hash(password, saltRounds);
+
         client.hset(`user:${userid}`, "hash", hash, "username", username);
+
+        saveSessionAndRenderDashboard(userid);
       });
     } else {
+      //here comes the login
       client.hget(`user:${userid}`, 'hash', async (err, hash) => {
         const result = await bcrypt.compare(password, hash); //returns true or false
         if (result) {
+          saveSessionAndRenderDashboard(userid)
           //then the PS is matching
         } else {
-          //the password is incorrect
+          res.render("error", {
+            message: "Password is incorrect",
+          })
+          return
         }
       })
     }
   })
-  res.end()
-  app.listen(3005, () => console.log("Server is ready Sir!"))
+  
 });
